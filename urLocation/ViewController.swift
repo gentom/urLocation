3f3cae65f1f62f1197aa9348dbaa871e5195376b
gentom <gento.m.developer@gmail.com>
2017-09-07 14:06:11 +0900

    commit

---
+++ urLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  urLocation
+//
+//  Created by Morikawa Gento on 2017/09/07.
+//  Copyright © 2017年 gentom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
