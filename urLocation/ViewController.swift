e21e7d8c3cf2a566ab8b5ca5c8deff12cb25f21c
gentom <gento.m.developer@gmail.com>
2017-09-07 14:35:28 +0900

    commit

--- urLocation/ViewController.swift
+++ urLocation/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    //Map
+    @IBOutlet weak var map: MKMapView!
+    
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +35,14 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01,0.01)
+        let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        self.map.showsUserLocation = true
+    }
+}
