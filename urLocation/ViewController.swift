ff2dd3bb124dc86b45520be226dac507986c5368
gentom <gento.m.developer@gmail.com>
2017-09-07 15:49:47 +0900

    commit

--- urLocation/ViewController.swift
+++ urLocation/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController {
     
     //Map
     @IBOutlet weak var map: MKMapView!
@@ -24,7 +24,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.distanceFilter = 100.0
-        manager.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        if(status == CLAuthorizationStatus.notDetermined) {
+            print("didChangeAuthorizationStatus:\(status)");
+            manager.requestWhenInUseAuthorization()
+        }
+        
         manager.startUpdatingLocation()
     }
 
@@ -33,6 +38,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    
+}
+
+
+extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
         let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01,0.01)
@@ -43,17 +53,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         self.map.setUserTrackingMode(MKUserTrackingMode.followWithHeading, animated: true)
     }
     
-}
-
-/*
-extension ViewController: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations[0]
-        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01,0.01)
-        let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
-        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
-        map.setRegion(region, animated: true)
-        self.map.showsUserLocation = true
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("didChangeAuthorizationStatus");
+        var statusStr = "";
+        switch (status) {
+        case .notDetermined:        statusStr = "NotDetermined"
+        case .restricted:           statusStr = "Restricted"
+        case .denied:               statusStr = "Denied"
+        case .authorized:           statusStr = "Authorized"
+        case .authorizedWhenInUse:  statusStr = "AuthorizedWhenInUse"
+        }
+        print(" CLAuthorizationStatus: \(statusStr)")
     }
 }
-*/
+
